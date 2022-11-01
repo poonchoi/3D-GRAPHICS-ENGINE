040d9ec1e111b2ba17c53ed6fdc9c9666afe09a0
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-11-01 22:25:45 +0100

    Create matrix.py

---
+++ library/matrix.py
@@ -0,0 +1,21 @@
+class mat():
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __mul__(self, other):
+        a = self.matrix
+        b = other
+        cols_a = len(a[0])
+        rows_a = len(a)
+        cols_b = len(b[0])
+        rows_b = len(b)
+
+        if cols_a == rows_b:
+            result = [[j for j in range(cols_b)] for i in range(rows_a)]
+            for i in range(rows_a):
+                for j in range(cols_b):
+                    sum = 0
+                    for k in range(cols_a):
+                        sum += a[i][k] * b[k][j]
+                    result[i][j] = sum
+            return result
