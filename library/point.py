2567173e9ee00d60ace8d416b352a23bb15ace1d
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-11-01 22:25:52 +0100

    Create point.py

---
+++ library/point.py
@@ -0,0 +1,9 @@
+class point:
+    def __init__(self, coordinate, angle_to_rotate=None, transform=None, scale=None):
+        self.coordinate = point
+    
+    def project(self):
+        pass
+
+
+point = point([1,2,3])
