142ea74ff8e36149fd999b31a3693f43caed2a51
Daniele Golzio <danielegolzio@gmail.com>
2022-11-22 22:58:56 +0100

    Update matrix.py

--- library/matrix.py
+++ library/matrix.py
@@ -1,6 +1,19 @@
-class mat():
+class Matrix():
     def __init__(self, matrix):
         self.matrix = matrix
+        self.cols = len(matrix)
+        self.rows = len(matrix[0])
+    
+    def __getitem__(self, index):
+        """
+        Defines behavior of using square brackets on Matrix objects
+
+        E.g:
+        > a = Matrix([1,2,3],[4,5,6])
+        > a[0]
+          [1,2,3]
+        """
+        return self.matrix[index]
 
     def __mul__(self, other):
         a = self.matrix
@@ -9,6 +22,10 @@ class mat():
         rows_a = len(a)
         cols_b = len(b[0])
         rows_b = len(b)
+        cols_a = self.cols
+        rows_a = self.rows
+        cols_b = other.cols
+        rows_b = other.rows
 
         if cols_a == rows_b:
             result = [[j for j in range(cols_b)] for i in range(rows_a)]
