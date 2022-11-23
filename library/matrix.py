f15414b3adb8bffc09163be97d5b742dfde906a2
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-11-23 10:46:06 +0100

    Update matrix.py

--- library/matrix.py
+++ library/matrix.py
@@ -1,9 +1,40 @@
+def zeroes():
+    """
+    Creates a matrix of size h x w and fills it with zeroes
+    """
+    pass
+
 class Matrix():
     def __init__(self, matrix):
         self.matrix = matrix
-        self.cols = len(matrix)
-        self.rows = len(matrix[0])
+        self.height = len(matrix)
+        self.width = len(matrix[0])
     
+    def __repr__(self):
+        """
+        Defines behaviour of printing a Matrix object
+        """
+        print("[",end="")
+        for height in range(self.height):
+            print("[",end="")
+            for width in range(self.width):
+                if width != self.width-1:
+                    print(f"{self.matrix[height][width]} ",end="")
+                else:
+                    print(f"{self.matrix[height][width]}",end="")
+            if height != self.height-1:
+                print("]")
+            else:
+                print("]",end="")
+        print("]",end="")
+        return ("")
+
+    def __rmul__(self, other):
+        """
+        Defines the behaviour when the matrix object is on the right of the * operator
+        """
+        pass
+
     def __getitem__(self, index):
         """
         Defines behavior of using square brackets on Matrix objects
@@ -13,22 +44,25 @@ class Matrix():
         > a[0]
           [1,2,3]
         """
-        return self.matrix[index]
+        return self.matrix[index] 
 
     def __mul__(self, other):
+        """
+        Defines the behaviour of the * operator
+        """
         a = self.matrix
         b = other
-        cols_a = len(a[0])
-        rows_a = len(a)
-        cols_b = len(b[0])
-        rows_b = len(b)
-        cols_a = self.cols
-        rows_a = self.rows
-        cols_b = other.cols
-        rows_b = other.rows
+        # cols_a = len(a[0])
+        # rows_a = len(a)
+        # cols_b = len(b[0])
+        # rows_b = len(b)
+        cols_a = self.width
+        rows_a = self.height
+        cols_b = other.width
+        rows_b = other.height
 
         if cols_a == rows_b:
-            result = [[j for j in range(cols_b)] for i in range(rows_a)]
+            result = Matrix([[j for j in range(cols_b)] for i in range(rows_a)]) # replace with zeroes function
             for i in range(rows_a):
                 for j in range(cols_b):
                     sum = 0
@@ -37,11 +71,28 @@ class Matrix():
                     result[i][j] = sum
             return result
 
-x = [1, 3, -5]
-y = [4, -2, -1]
+x = Matrix([[1,2,3]])
+y = Matrix([[1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]])
+
+result = x*y
+print(result)
+
+### TESTING MATRIX MULTIPLICATION FUNCTION ###
+
+x = [[12, 0],
+     [0, 13]]
+y = [[4, 1],
+     [1, 5]]
+
+d = [[1, 2, 3]]
+g = [[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 0]]
 
 def dot(a, b):
-    cols_a = len(a[0])
+    cols_a = len(a[0])  
     rows_a = len(a)
     cols_b = len(b[0])
     rows_b = len(b)
@@ -56,4 +107,7 @@ def dot(a, b):
                 result[i][j] = sum
         return result
 
-print(dot(x, y))
+# print(dot(x, y))
+# print(dot(d, g))
+
+##############################################
