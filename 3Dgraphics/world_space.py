1359856ee9f66d56a2d225906cce637e1668f9f9
Daniele Golzio <danielegolzio@gmail.com>
2022-12-01 01:02:22 +0100

    Update world_space.py

--- 3Dgraphics/world_space.py
+++ 3Dgraphics/world_space.py
@@ -1,21 +1,44 @@
+from MatrixMath.matrix import Matrix
+
 class World_Space:
     def __init__(self):
         self.all_shapes = []
+        self.__PROJECT_MATRIX = Matrix([[1, 0, 0],
+                                      [0, 1, 0]
+                                      [0, 0, 0]])
+
+
     def add_shape(self, shape):
         pass
-    def project(self):
-        pass
+
+
+    def project(self, point):
+        return point * self.__PROJECT_MATRIX
+
+    
     def rotate_x(self, angle):
         pass
+
+
     def rotate_y(self, angle):
         pass
+
+
     def rotate_z(self, angle):
         pass
+
+
     def translate(self, new_pos):
         pass
+
+
     def draw(self):
         pass
+
+
     def check_movement(self):
         pass
+
+
     def run(self):
         pass
