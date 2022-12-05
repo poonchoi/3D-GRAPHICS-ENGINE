aa15dbd562dd7fb07d99ca1998fff411e10ef0ea
Daniele Golzio <danielegolzio@gmail.com>
2022-12-06 00:10:12 +0100

    testing how the library works

--- _librarytest.py
+++ _librarytest.py
@@ -1,6 +1,6 @@
-from graphics import app, point
+from pygame3D import pg3d
 
-app = app.App(300, 300)
-new_point = point.Point(app, [1, 2, 3])
+app = pg3d.App(300, 300)
+new_point = pg3d.Point(app, [1, 2, 3])
 
 app.run()
