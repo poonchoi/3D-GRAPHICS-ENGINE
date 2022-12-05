58e9f8810bfab09c82d9cca798cba57ca75bfeda
Daniele Golzio <danielegolzio@gmail.com>
2022-12-06 00:13:33 +0100

    moved tests to single folder

--- pygame3D/pg3d.py
+++ pygame3D/pg3d.py
@@ -11,7 +11,6 @@ class App:
         self.clock = pg.time.Clock()
         self.world = World(self.RES, self.screen)
 
-
     def run(self):
         while True:
             World.draw()
@@ -20,6 +19,7 @@ class App:
             pg.display.set_caption(f"{round(self.clock.get_fps())} FPS")
             pg.display.flip()
             self.clock.tick(self.FPS)
+
 
 class Point():
     projection_matrix = Matrix([[1, 0, 0],
@@ -35,6 +35,7 @@ class Point():
     def __repr__(self):
         return str(self.coordinate[0])
 
+
 class World():
     def __init__(self, dimensions, screen):
         self.dimensions = dimensions
@@ -44,18 +45,14 @@ class World():
                                         [0, 1, 0]
                                         [0, 0, 0]])
 
-
     def add_point(self, point):
         self.all_shapes.append([point])
-
 
     def add_shape(self, shape):
         self.all_shapes.append(shape)
 
-
     def project(self, point):
         return point * self._PROJECT_MATRIX
-
 
     def rotate_x(self, angle, point):
         rotateX = Matrix([[1, 0, 0],
@@ -63,13 +60,11 @@ class World():
                           [0, -m.sin(angle), m.cos(angle)]])
         return point * rotateX
 
-
     def rotate_y(self, angle, point):
         rotateY = Matrix([[m.cos(angle), 0, -m.sin(angle)],
                           [0, 1, 0],
                           [m.sin(angle), 0, m.cos(angle)]])
         return point * rotateY
-
 
     def rotate_z(self, angle, point):
         rotateZ = Matrix([[m.cos(angle), m.sin(angle), 0],
@@ -77,14 +72,12 @@ class World():
                           [0, 0, 1]])
         return point * rotateZ
 
-
     def translate(self, new_pos, point):
         x, y, z = new_pos
         translate = Matrix([[x, 0, 0],
                             [0, y, 0],
                             [0, 0, z]])
         return point * translate
-
 
     def draw(self):
         projected_points = [[n, n] for n in range(len(self.self.all_shapes))]
@@ -95,10 +88,8 @@ class World():
                 x, y, z = projected
                 pg.draw.circle(self.screen, (0,255,0), (projected))
 
-
     def check_movement(self):
         key = pg.key.get_pressed()
-
 
     def run(self):
         pass
