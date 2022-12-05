e20b5582c1496ad97950eda7bde2fb9abca694ed
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-12-05 15:22:59 +0100

    Update point.py

--- 3Dgraphics/point.py
+++ 3Dgraphics/point.py
@@ -1,19 +1,19 @@
 from MatrixMath.matrix import Matrix
 from world import World
+import math as m
 
-class Point():
+class Point(World):
+    projection_matrix = Matrix([[1, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, 0]])
     def __init__(self, world, coordinate):
         self.coordinate = Matrix([coordinate])
         world.add_point()
+    
+    def project(self):
+        return self.coordinate * Point.projection_matrix
 
     def __repr__(self):
         return str(self.coordinate[0])
 
-points = [[[32],[3]], [[3],[2]], [[32],[12]]]
-projected_points = [[n, n] for n in range(len(points))]
-print(points)
-print(projected_points)
-for shape in points:
-    for point in shape:
-        print(point)
-        projected_points[shape][point] = [1,2,3]
+xyz = Point([1, 2, 3])
