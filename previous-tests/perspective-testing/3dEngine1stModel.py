9fc47939a8087ac761d4efacd0c0cb1c9ba2c53e
Daniele Golzio <80271815+poonchoi@users.noreply.github.com>
2022-12-13 09:58:37 +0100

    Update 3dEngine1stModel.py

--- previous-tests/perspective-testing/3dEngine1stModel.py
+++ previous-tests/perspective-testing/3dEngine1stModel.py
@@ -15,7 +15,7 @@ MAGENTA = (255, 0, 255)
 class App:
     def __init__(self):
         pg.init()
-        self.RES = self.w, self.HEIGHT = 600, 600
+        self.RES = self.WIDTH, self.HEIGHT = 600, 600
         self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
         self.FPS = 100
         self.screen = pg.display.set_mode(self.RES)
@@ -58,7 +58,7 @@ class App:
         return rx
 
 
-    def ry(self)
+    def ry(self):
         ry = np.array([
             [np.cos(self.rot[1]),0,np.sin(self.rot[1]),0],
             [0,1,0,0],
@@ -78,47 +78,32 @@ class App:
         return rz
 
 
-    def c(self, point):
-        cx = point[0]
-        cy = point[1]
-        cz = point[2]
-        c = np.matrix([[1, 0, 0, -cx],
-        [0, 1, 0, -cy],
-        [0, 0, 1, -cz],
-        [0, 0, 0, 1]])
-        return c
-
-    def project(self):
-        result = []
-        n = np.matrix([
-            [1/result]
+    def g(self, point):
+        G = np.array([
+            [1,0,0,-point[0]],
+            [0,1,0,-point[1]],
+            [0,0,1,-point[2]],
+            [0,0,0,1]
         ])
+
+        return G
+
+
+    def project(self, point):
+        new_point = point @ self.p() @ self.rx() @ self.ry() @ self.rx() @ self.g(point)
+        proj = [None, None]
+        proj[0] = new_point[0] / new_point[3]
+        proj[1] = new_point[1] / new_point[3]
 
 
     def draw(self):
-        # self.angle = pg.mouse.get_pos()[0] / 1000
-        self.angle += 0.01
         self.screen.fill(WHITE)
-        points = [[3, -1, 3], [5, -1, 3], [5, 1, 3], [3, 1, 3], [3, -1, -3], [5, -1, -3], [5, 1, -3], [3, 1, -3]]
+        points = [[0,0,0,1],[-1,1,1,1],[1,1,1,1],[-1,-1,1,1],[1,-1, 1,1],[-1,1,-1,1],[1,1,-1,1],[-1,-1,-1,1],[1,-1,-1,1]]
         #[0,0,0],[-1,1,1],[1,1,1],[-1,-1,1],[1,-1, 1],[-1,1,-1],[1,1,-1],[-1,-1,-1],[1,-1,-1]
-        projected_points = [[n, n] for n in range(len(points))]
-        i = 0
         for point in points:
-            rotated = self.rotateY(point, self.angle+0.01)
-            #rotated = self.rotateX(rotated, self.angle)
-            #rotated = self.translate(rotated, [0,0,-self.angle*5])
-            projected = self.project(rotated)
-            x = projected[0] * self.scale + self.H_WIDTH
-            y = projected[1] * self.scale + self.H_HEIGHT
-            projected_points[i] = x, y
+            projected = self.project(point)
+            x, y = projected[0], projected[1]
             pg.draw.circle(self.screen, BLACK, (x, y), 2)
-            i += 1
-
-        
-        for p in range(4):
-            self.connect_points(p, (p+1) % 4, projected_points)
-            self.connect_points(p+4, ((p+1) % 4) + 4, projected_points)
-            self.connect_points(p, (p+4), projected_points) 
 
 
     def run(self):
